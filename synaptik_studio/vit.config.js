fe079c4e3a1962027ae09249a5e1df776b0ea921
legiongirl113 <legiongirl113@gmail.com>
2025-12-04 20:36:11 -0500

    Add Firebase config and app ID to vit.config.js
    Added Firebase configuration and app ID to Vite config.

--- synaptik_studio/vit.config.js
+++ synaptik_studio/vit.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // These fix the variable errors in the main app code
+    __firebase_config: JSON.stringify("{}"),
+    __app_id: JSON.stringify("synaptik-v1"),
+    __initial_auth_token: JSON.stringify("")
+  }
 })
